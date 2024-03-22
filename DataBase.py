7f8d98e40cd694d0b0ad744ea4be2e827b960f96
Sashoks007 <160155406+Sashoks007@users.noreply.github.com>
2024-03-22 15:26:32 +0300

    Add files via upload

--- DataBase.py
+++ DataBase.py
@@ -1,21 +1,19 @@
-
-import tkinter as tk
-import tkinter.messagebox
 import pymysql.cursors
+import tkinter.messagebox
+class DataBase:
+    def __init__(self, db, tb):
+        self.name_db = db
+        self.name_tb = tb
 
-class DatabaseCreation:
-    def init(self, db_name):
-        self.db_name = db_name
-
-    def create_database(self):
+    def check_db(self):
         try:
             conn = pymysql.connect(host="localhost",
                                    user="root",
                                    password="root",
-                                   database=self.db_name,
+                                   database=self.name_db,
                                    cursorclass=pymysql.cursors.Cursor)
             print("Вы подключились")
-            tkinter.messagebox.showinfo("Вы подключились", self.db_name)
+            tkinter.messagebox.showinfo("Вы подключились", self.name_db)
         except pymysql.err.MySQLError:
             conn = pymysql.connect(host="localhost",
                                    user="root",
@@ -23,92 +21,45 @@ class DatabaseCreation:
                                    cursorclass=pymysql.cursors.Cursor)
 
             cursor = conn.cursor()
-            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.db_name}")
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.name_db}")
             print("Вы создали БД")
-            tkinter.messagebox.showinfo("Вы создали БД", self.db_name)
+            tkinter.messagebox.showinfo("Вы создали БД", self.name_db)
         return conn
 
-    def list_databases(self):
-        connection = self.create_database()
-        cursor = connection.cursor()
-        cursor.execute("SHOW DATABASES;")
-        databases = cursor.fetchall()
+    def con_db(self):
+        return pymysql.connect(host="localhost",
+                               user="root",
+                               password="root",
+                               database=self.name_db,
+                               cursorclass=pymysql.cursors.Cursor)
 
-        db_list = [db[0] for db in databases]
-        db_list_str = "\\n".join(db_list)
-
-        tkinter.messagebox.showinfo("Список баз данных", db_list_str)
-
-class TableCreation:
-    def init(self, db_name, tb_name):
-        self.db_name = db_name
-        self.tb_name = tb_name
-
-    def create_table(self):
-        connection = pymysql.connect(host="localhost",
-                                    user="root",
-                                    password="root",
-                                    database=self.db_name,
-                                    cursorclass=pymysql.cursors.Cursor)
+    def check_table(self):
+        connection = self.con_db()
         cursor = connection.cursor()
         try:
-            cursor.execute(f"SELECT * FROM {self.tb_name}")
+            cursor.execute(f"SELECT * FROM {self.name_db}")
             print("Таблица подключена")
-            tkinter.messagebox.showinfo("Таблица подключена", self.tb_name)
+            tkinter.messagebox.showinfo("Таблица подключена", self.name_tb)
         except pymysql.err.MySQLError:
-            cursor.execute(
-                f"CREATE TABLE IF NOT EXISTS {self.tb_name} (id bigint NOT NULL AUTO_INCREMENT,"
-                f" list varchar(1000) NOT NULL,"
-                f" indexes varchar(1000) NOT NULL,"
-                f" PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;")
-            connection.commit()
-            print("Таблица создана и подключена")
-            tkinter.messagebox.showinfo("Таблица создана и подключена", self.tb_name)
 
-    def list_tables(self):
-        connection = pymysql.connect(host="localhost",
-                                    user="root",
-                                    password="root",
-                                    database=self.db_name,
-                                    cursorclass=pymysql.cursors.Cursor)
+            cursor.execute(
+                f"CREATE TABLE IF NOT EXISTS {self.name_tb} (id bigint NOT NULL AUTO_INCREMENT,"
+                f" list varchar(1000) NOT NULL,"
+                f"indexi varchar(1000) NOT NULL,"
+                f" PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;")
+        connection.commit()
+        print("Таблица создана и подключена")
+        tkinter.messagebox.showinfo("Таблица создана и подключена", self.name_tb)
+        return
+
+    def list_tb(self):
+        connection = self.con_db()
         cursor = connection.cursor()
-        cursor.execute("SHOW TABLES;")
+        tb_in_db = "SHOW TABLES;"
+        cursor.execute(tb_in_db)
         tables = cursor.fetchall()
 
         table_list = [table[0] for table in tables]
-        table_list_str = "\\n".join(table_list)
+        table_list_str = "\n".join(table_list)
 
         tkinter.messagebox.showinfo("Список таблиц", table_list_str)
-
-class App(tk.Tk):
-    def init(self):
-        super().init()
-        self.title("Создание базы данных и таблицы")
-
-        self.create_db_label = tk.Label(self, text="Имя базы данных:")
-        self.create_db_label.pack()
-        self.create_db_entry = tk.Entry(self, textvariable=tk.StringVar())
-        self.create_db_entry.pack()
-
-        self.create_db_button = tk.Button(self, text="Создать базу данных", command=self.create_database)
-        self.create_db_button.pack()
-
-        self.create_table_label = tk.Label(self, text="Имя таблицы:")
-        self.create_table_label.pack()
-        self.create_table_entry = tk.Entry(self, textvariable=tk.StringVar())
-        self.create_table_entry.pack()
-        self.create_table_button = tk.Button(self, text="Создать таблицу", command=self.create_table)
-        self.create_table_button.pack()
-
-    def create_database(self):
-        db_creator = DatabaseCreation(self.create_db_entry.get())
-        db_creator.create_database()
-
-    def create_table(self):
-        table_creator = TableCreation(self.create_db_entry.get(), self.create_table_entry.get())
-        table_creator.create_table()
-
-app = App()
-app.mainloop()
-
-
--- main.py
+++ main.py
@@ -1,43 +1,180 @@
-import tkinter as tk
+
 import tkinter.messagebox
+import tkinter as tk
+import random
+import string
+import openpyxl
 import pymysql.cursors
+from openpyxl.utils import get_column_letter
+from openpyxl.utils.dataframe import dataframe_to_rows
 
-class ListInput:
-    def __init__(self, db_name, tb_name):
-        self.db_name = db_name
-        self.tb_name = tb_name
+import DataBase
+import pandas as pd
 
-    def input_list_and_save(self):
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
         input_window = tk.Tk()
         input_window.title("Ввод списка")
 
-        list_label = tk.Label(input_window, text="Введите список из 95 элементов через пробел:")
-        list_label.pack()
+        self.db_name = None
+        self.table_name = None
+        self.title("Создание базы данных")
 
-        list_entry = tk.Entry(input_window, width=50)
-        list_entry.pack()
+        self.db_label = tk.Label(self, text="Имя базы данных:")
+        self.db_label.pack()
+        self.db_entry = tk.Entry(self, textvariable=tk.StringVar())
+        self.db_entry.pack()
 
-        def show_even_indices():
-            input_list = list(map(int, list_entry.get().split()))
-            even_indices = [str(i) for i in range(len(input_list)) if input_list[i] % 2 == 0]
-            even_indices_str = "\n".join(even_indices)
+        self.table_label = tk.Label(self, text="Имя таблицы:")
+        self.table_label.pack()
+        self.table_entry = tk.Entry(self, textvariable=tk.StringVar())
+        self.table_entry.pack()
 
-            result_window = tk.Tk()
-            result_window.title("Индексы четных элементов")
-            result_label = tk.Label(result_window, text=even_indices_str)
-            result_label.pack()
+        self.list_label = tk.Label(input_window, text="Введите 85 отрицательных и положительных элементов:")
+        self.list_label.pack()
+        self.list_entry = tk.Entry(input_window, width=50)
+        self.list_entry.pack()
 
-            connection = pymysql.connect(host="localhost",
-                                        user="root",
-                                        password="root",
-                                        database=self.db_name,
-                                        cursorclass=pymysql.cursors.Cursor)
-            cursor = connection.cursor()
-            cursor.execute(f"INSERT INTO {self.tb_name} (list, indexes) VALUES ('{list_entry.get()}', '{even_indices_str}')")
-            connection.commit()
+        self.title("Записи из таблицы MySQL")
+        self.records_text = tk.Text(self)
+        self.records_text.pack()
 
-        show_button = tk.Button(input_window, text="Показать индексы четных элементов", command=show_even_indices)
-        show_button.pack()
+        self.file1_label1 = tk.Label(self, text="Имя файла эксель:")
+        self.file1_label1.pack()
+        self.file1_entry1 = tk.Entry(self, textvariable=tk.StringVar())
+        self.file1_entry1.pack()
+
+        self.db_label.pack(padx=50, pady=2)
+        self.table_label.pack(padx=50, pady=2)
+        self.file1_label1.pack(padx=50, pady=2)
+
+    def create_database(self):
+        self.db_name = self.db_entry.get()
+        self.table_name = self.table_entry.get()
+
+        db = DataBase.DataBase(self.db_name, self.table_name)
+        db.check_db()
+        db.check_table()
+        self.mysql_tb()
+
+    def rand_alf_sample(self):
+        gl = 'aeyuio'
+        list_alf = [str(i) for i in self.sample_120_entry.get().split()]
+        #list_alf = [random.choice(string.ascii_lowercase) for _ in range(120)]
+        count_gl = 0
+        count_sogl = 0
+        for i in list_alf:
+            if i in gl:
+                count_gl += 1
+            else:
+                count_sogl += 1
+        return list_alf, count_gl, count_sogl
+
+    def separate_numbers(self, numbers):
+        positive_numbers = [num for num in numbers if num > 0]
+        negative_numbers = [num for num in numbers if num < 0]
+        return positive_numbers, negative_numbers
+
+
+    def inp_list(self):
+        numbers = self.list_entry.get().split()
+        positive_numbers, negative_numbers = self.separate_numbers(numbers)
+    def mysql_tb(self):
+        db1 = DataBase.DataBase(self.db_name, self.table_name)
+        connection = db1.con_db()
+        with connection.cursor() as cursor:
+            sql = f"SELECT * FROM {self.table_name}"
+            cursor.execute(sql)
+            records = cursor.fetchall()
+
+            for record in records:
+                self.records_text.insert(tk.END, f"{record}\n")
+
+    def save_result(self):
+        input_list, even_indices_str = self.inp_list()
+        db1 = DataBase.DataBase(self.db_name, self.table_name)
+        connection = db1.con_db()
+
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(f"INSERT INTO {db1.name_tb} (list_ind, indexi) VALUES (%s, %s)",
+                               (str(input_list), str(even_indices_str)))
+                connection.commit()
+
+                cursor.execute(f"SELECT * FROM {db1.name_tb}")
+                print(cursor.fetchall()[-1])
+
+        except pymysql.err.DataError as e:
+            print('Ошибка с данными:', e)
+
+        except pymysql.err.DatabaseError as e:
+            print(e)
+
+    def list_tb(self):
+        db1 = DataBase.DataBase(self.db_name, self.table_name)
+        connection = db1.con_db()
+        cursor = connection.cursor()
+        tb_in_db = "SHOW TABLES;"
+        cursor.execute(tb_in_db)
+        tables = cursor.fetchall()
+
+        table_list = [table[0] for table in tables]
+        table_list_str = "\n".join(table_list)
+
+        tkinter.messagebox.showinfo("Список таблиц", table_list_str)
+
+
+    def save_to_excel(self):
+        db1 = DataBase.DataBase(self.db_name, self.table_name)
+        connection = db1.con_db()
+        try:
+            new_df = pd.read_sql("SELECT * FROM " + self.table_name, connection)
+            wb = openpyxl.Workbook()
+            ws = wb.active
+
+            for r in dataframe_to_rows(new_df, index=False, header=True):
+                ws.append(r)
+
+            for column in ws.columns:
+                max_length = 0
+                column_letter = get_column_letter(column[0].column)
+                for cell in column:
+                    try:
+                        if len(str(cell.value)) > max_length:
+                            max_length = len(cell.value)
+                    except TypeError:
+                        pass
+
+                adjusted_width = (max_length + 2) * 1.2
+                ws.column_dimensions[column_letter].width = adjusted_width
+            file1 = self.file1_entry1.get()
+            wb.save(file1)
+            print(new_df)
+
+            tkinter.messagebox.showinfo("Импорт в эксель", file1)
+
+        except pymysql.err.DatabaseError as e:
+            print(e)
+        return
+
 
 app = App()
+
+create_button1 = tk.Button(app, text="Создать БД", command=app.create_database)
+create_button1.pack()
+
+button = tk.Button(app, text="Список", command=app.inp_list)
+button.pack()
+
+create_button = tk.Button(app, text="Создать запись", command=app.save_result)
+create_button.pack()
+
+list_button = tk.Button(app, text="Показать список таблиц", command=app.list_tb)
+list_button.pack()
+
+excel_button = tk.Button(app, text="Импорт в эксель", command=app.save_to_excel)
+excel_button.pack()
+
+
 app.mainloop()
